9b85e1ee91ccad83938e07e80b50adf89eac0b78
thayoon <83504880+thayoon@users.noreply.github.com>
2021-05-12 14:54:19 +0900

    회원가입 페이지 바인딩

--- accountapp/views.py
+++ accountapp/views.py
@@ -33,7 +33,7 @@ def hello_world(request):
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('goapp:rating')
     template_name = 'accountapp/create.html'
 
 
