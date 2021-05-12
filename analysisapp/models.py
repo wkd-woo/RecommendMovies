212e8fedf3a494738b8977e0a2af51e3c375c3b7
wkd-woo <wkdwoos@gmail.com>
2021-05-12 18:41:24 +0900

    DB data export successfully and now importing ml

--- analysisapp/models.py
+++ analysisapp/models.py
@@ -1,17 +1,31 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.forms import ValidationError
+
 
 # Create your models here.
 
-class Rating(models.Model): # 평점 모델.
-    rating_id = models.AutoField(primary_key=True) # rating_gid, pk
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE) # 내부 DB에 있는 USER 연동. foreignKey
-    movie_id = models.IntegerField() # 외부 DB에 있는 movies 연동. foreignKey
-    rating = models.FloatField(default=2.5, verbose_name='평점') # 평점
-    timestamp = models.DateTimeField(auto_now_add=True) # 해당 레코드 생성시 현재 시간 자동 저장
-    comment = models.CharField(max_length=240, verbose_name='코멘트', help_text='코멘트를 입력해 주세요. 240자 제한.') # 240자 제한
+class Rating(models.Model):  # 평점 모델.
+    rating_id = models.AutoField(primary_key=True)  # rating_gid, pk
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)  # 내부 DB에 있는 USER 연동. foreignKey
+    movie_id = models.IntegerField()  # 외부 DB에 있는 movies 연동. foreignKey
+    rating = models.FloatField(default=2.5, verbose_name='평점')  # 평점
+    timestamp = models.DateTimeField(auto_now_add=True)  # 해당 레코드 생성시 현재 시간 자동 저장
+    comment = models.CharField(max_length=240, verbose_name='코멘트', help_text='코멘트를 입력해 주세요. 240자 제한.', null=True)  # 240자 제한
 
 
-#class RateManager():
-   # def insert_rating(self, user_id, movie_id, rating):
+def rateManager(self, user_id, movie_id, rating, comment):
+    """
+    평점 모델 manager
+    """
+
+    def insert_rating(self, r_model, user_id, movie_id, rating, comment):
+        # 테이블 입력
+        r_model.user_id = user_id
+        r_model.movie_id = movie_id
+        r_model.rating = rating
+        r_model.comment = comment
+        return r_model
+
+    insertedRate = Rating() # model 생성 후
+    insertedRate = insert_rating(insertedRate, user_id, movie_id, rating, comment) # 테이블 입력!
+    return insertedRate # 반환
--- analysisapp/predict.py
+++ analysisapp/predict.py
@@ -1,97 +1,35 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import pymysql
 import seaborn as sns
 from sklearn.linear_model import Lasso
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import uniform as sp_rand
 import pickle
 import os
 import warnings
 
-currentpath = os.getcwd()
-warnings.filterwarnings('ignore')
 
-rating_path = 'analysisapp/data/ratings.csv'
-my_rating_path = 'analysisapp/data/my_ratings_input.csv'
-movie_path = 'analysisapp/data/movies.csv'
-genre_path = 'analysisapp/data/genres.p'
+# DB 연결
+connToRating = pymysql.connect(host="localhost", user="root", password="1234",
+                      db="rating_data", charset="utf8")
+cursor = connToRating.cursor(pymysql.cursors.DictCursor)
 
-ratings = pd.read_csv(rating_path)
-my_ratings = pd.read_csv(my_rating_path)
-movies = pd.read_csv(movie_path)
-genres = pd.read_pickle(genre_path)
+query = "SELECT * FROM ratings"
+cursor.execute(query)
 
-my_ratings.shape
-
-my_ratings = my_ratings.merge(movies, on='movieId').merge(genres, left_on='movieId', right_index=True)
-
-my_ratings.shape
-my_ratings.sample()
-
-"""## user1002"""
-
-user1002 = my_ratings[my_ratings['userId'] == 1002]
-
-user1002.shape
-
-user1002['rating'].hist()
-
-user1002['rating'].value_counts()
-
-user1002['rating'].describe()
-
-user1002[user1002['rating']==2.0]
-
+ratings = pd.read_sql(query, connToRating)
+my_ratings = pd.read_csv('analysisapp/data/my_ratings_input.csv')
+movies = pd.read_csv('analysisapp/data/movies.csv')
+genres = pd.read_pickle('analysisapp/data/genres.p')
 genre_cols = genres.columns
 
-user1002[genre_cols].sum().sort_values(ascending=False)
-
-"""## 나와 친구의 취향 차이"""
-
-my_ratings['movieId'].value_counts().sort_values(ascending=False)
-
-my_ratings[my_ratings['movieId'] == 97938]
-
-"""호불호가 가장 심한 장르를 찾아보자
- - 같이 영화를 보려면 반드시 피해야 하는 장르
-"""
-
-my_ratings_v1 = my_ratings.copy()
-my_ratings_v1 = my_ratings_v1.replace(0, np.nan)
-
-for col in genre_cols:
-  my_ratings_v1[col] = my_ratings_v1[col] * my_ratings_v1['rating']
-
-user_profile_v1 = my_ratings_v1.groupby('userId')[genre_cols].mean()
-user_profile_v1
-
-user_profile_describe = user_profile_v1.describe()
-user_profile_describe
-
-user_profile_describe.loc[:, user_profile_describe.loc['count'] == 2]
-
-thriller = my_ratings[my_ratings['Thriller'] == 1]
-
-sns.boxplot(data=thriller, x='userId', y='rating')
-
-"""## Lasso로 user profile 만들기"""
 
 model = Lasso()
-model
-# alpha : 정규화 텀 (계수의 크기를 조절하는 것)
-
-"""### 하이퍼 파라미터 튜닝
-
-"""
-
-from sklearn.model_selection import RandomizedSearchCV
-from scipy.stats import uniform as sp_rand
-
 param_grid = {'alpha': sp_rand()}
-
 rsearch = RandomizedSearchCV(estimator=model, param_distributions=param_grid, n_iter=200, cv=20, random_state=42)
 
-user1001 = my_ratings[my_ratings['userId'] == 1001]
-user1002 = my_ratings[my_ratings['userId'] == 1002]
 user1003 = my_ratings[my_ratings['userId'] == 1003]
 
 rsearch.fit(user1003[genre_cols], user1003['rating'])
@@ -102,19 +40,12 @@ intercept = rsearch.best_estimator_.intercept_
 coef = rsearch.best_estimator_.coef_
 
 user1003_profile = pd.DataFrame([intercept, *coef], index=['intercept', *genre_cols], columns=['score'])
-user1003_profile
-
-plt.figure(figsize=(25, 7))
-sns.barplot(data=user1003_profile.reset_index(), x='index', y='score')
 
 predictions = rsearch.best_estimator_.predict(genres)
-predictions
-
 genres['user1003'] = predictions
 
 rating_predictions = genres[~genres.index.isin(user1003['movieId'])].sort_values('user1003', ascending=False)
-rating_predictions.head()
 
 rating_predictions = rating_predictions.merge(movies[['movieId', 'title']], left_index=True, right_on='movieId')
-rating_predictions
 
+print(rating_predictions)
