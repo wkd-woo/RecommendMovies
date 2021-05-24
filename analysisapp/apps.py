7810edbab45b95deb6bc41d47872cca10a01d241
wkd-woo <wkdwoos@gmail.com>
2021-05-24 18:54:27 +0900

    from now on ml model is in apps.py and predict is updated(worst, genre)

--- analysisapp/apps.py
+++ analysisapp/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from sklearn.linear_model import Lasso
 
 
 class AnalysisappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'analysisapp'
+    model = Lasso() # 장고 실행 시 모델을 app과 함께 한 번만 불러옴 : 속도 개선
