f9e437455a7764e56835b8116f336ea8cec488f4
thayoon <thagyun@naver.com>
2021-05-31 01:38:59 +0900

    평가페이지+상세페이지
    평가페이지에서 모든 영화 출력
    영화 제목을 클릭하면 상세페이지로 이동

--- goapp/views.py
+++ goapp/views.py
@@ -3,6 +3,7 @@ from .models import Movie
 import csv
 import pandas as pd
 from analysisapp.models import Results
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -30,9 +31,29 @@ def rating_home(request):
     Movie.objects.bulk_create(list)
 
     return HttpResponse('create model ')"""
-    movie_list = Movie.objects.all().filter(imgurl__contains='/')[:30]
+    """movie_list = Movie.objects.all().filter(imgurl__contains='/')[:30]
     # Movie 모델의 imgurl모델에서 '/'문자열을 포함하는 30개의 데이터 조회
     context = {
         'movie_list': movie_list
+    }"""
+    # 입력 파라미터
+    page = request.GET.get('page', '1') # 페이지
+
+    # 조회
+    movie_list = Movie.objects.all()[1:]
+
+    # 페이징처리
+    paginator = Paginator(movie_list, 30)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {
+        'movie_list': page_obj
     }
+    # 저장된 모든 영화 불러오기
     return render(request, 'goapp/rating.html', context)
+
+def rating_detail(request, movie_id):
+    movie = Movie.objects.get(movieId=movie_id)
+    context = {
+        'movie':movie
+    }
+    return render(request, 'goapp/rating_detail.html', context)
